284a53a40d25858ff3aacec36134a0b21376dc17
Steve Olufemi <solufemi-olukanni@hotmail.com>
2021-03-20 13:53:51 +0000

    Add project files.

---
+++ Price-Calculator-Kata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Price_Calculator_Kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
